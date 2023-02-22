0f6e820d67b020bb8efe2c8a543d535d47d1e148
Sekkacchi <sekkacchi@gmail.com>
2023-02-22 10:36:02 -0600

    Add poppins font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,104 @@
 import * as React from "react";
+import localFont from "@next/font/local";
 import type { AppProps } from "next/app";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 import * as Colors from "@/utils/colors";
+
+const poppins = localFont({
+  src: [
+    {
+      path: "./fonts/Poppins-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Poppins-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Poppins-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+});
 
 const GlobalStyles = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
@@ -31,6 +127,7 @@ const GlobalStyles = createGlobalStyle`
     max-width: 100vw;
     color: ${(props: ThemeProp) => props.theme.text};
     background-color: ${(props: ThemeProp) => props.theme.backgroundTwo};
+    font-family: ${poppins.style.fontFamily};
     overflow-x: hidden;
   }
 
@@ -131,7 +228,7 @@ const themes: Themes = {
     warning: Colors.warning.light,
     shadowOne: Colors.shadow.light.one,
     shadowTwo: Colors.shadow.light.two,
-    shadowOverlay: Colors.shadowOverlay.light
+    shadowOverlay: Colors.shadowOverlay.light,
   },
   dark: {
     text: Colors.text.dark,
@@ -156,7 +253,7 @@ const themes: Themes = {
     warning: Colors.warning.dark,
     shadowOne: Colors.shadow.dark.one,
     shadowTwo: Colors.shadow.dark.two,
-    shadowOverlay: Colors.shadowOverlay.dark
+    shadowOverlay: Colors.shadowOverlay.dark,
   },
 };
 
