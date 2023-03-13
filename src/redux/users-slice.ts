14a4f5ba38e1c05d48fc9b2fbf9d343e6f97d0e9
Sekkacchi <sekkacchi@gmail.com>
2023-03-13 14:46:48 -0500

    Add database types to entities slice.

--- src/redux/users-slice.ts
+++ src/redux/users-slice.ts
@@ -3,6 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 import { Theme } from "./ui-slice";
 
+// ========================================================================================= //
+// [ TYPES ] =============================================================================== //
+// ========================================================================================= //
+
+type Dates = {
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 type User = {
   id: number;
   email: string;
@@ -16,9 +25,56 @@ type User = {
   emailVerified: boolean;
   loggedIn: boolean;
   signedVerificationCode?: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  overview: Overview[];
+  logbooks: Logbook[];
+} & Dates;
+
+type Category = "need" | "planned" | "impulse" | "regret";
+type Purchase = {
+  id: number;
+  placement: number;
+  category?: Category;
+  description?: string;
+  cost?: number;
+  overviewGroup: OverviewGroup;
+  logbookEntry: LogbookEntry;
+} & Dates;
+
+type Overview = {
+  id: number;
+  income: number;
+  savings: number;
+  groups: OverviewGroup[];
+  ownerId: number;
+} & Dates;
+
+type OverviewGroup = {
+  id: number;
+  name: string;
+  totalCost: number;
+  purchases: Purchase[];
+  overviewId: number;
+} & Dates;
+
+type Logbook = {
+  id: number;
+  name: string;
+  entries: LogbookEntry[];
+  ownerId: number;
+} & Dates;
+
+type LogbookEntry = {
+  id: number;
+  date: Date;
+  spent: number;
+  budget?: number;
+  purchases: Purchase[];
+  logbookId: number;
+} & Dates;
+
+// ========================================================================================= //
+// [ SLICE ] =============================================================================== //
+// ========================================================================================= //
 
 type EntitiesState = {
   user: User | null;
