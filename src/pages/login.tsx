964a783acdd1c447b98091227a339b66f9d9471d
Sekkacchi <sekkacchi@gmail.com>
2023-03-17 10:14:00 -0500

    Set up Login page.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,17 @@
-import { useSignal } from "@preact/signals-react";
+import { useSelector } from "react-redux";
 
 import * as Components from "@/components/auth";
+import { GlobalState } from "@/store";
 
 const Login = () => {
-  const toConfirmation = useSignal(false);
+  const { verificationCodeSent } = useSelector(
+    (state: GlobalState) => state.ui
+  );
 
-  return (
-    <Components.AuthForm
-      title="Log In"
-      caption="Don't have an account?"
-      toConfirmation={() => (toConfirmation.value = true)}
-    />
+  return !verificationCodeSent ? (
+    <Components.AuthForm title="Log In" caption="Don't have an account?" />
+  ) : (
+    <Components.Verification title="Verify Login" />
   );
 };
 
