88f5f79e3cc3fcc5aa5cfdf5a30ba55577103924
Sekkacchi <sekkacchi@gmail.com>
2023-04-01 10:24:10 -0500

    Refactor redux directory index file with cleaner export syntax.

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,18 +1,3 @@
-import { UIState, uiActions, uiReducer } from "./ui-slice";
-import {
-  EntitiesState,
-  entitiesActions,
-  entitiesReducer,
-} from "./entities-slice";
-import { ErrorsState, errorsActions, errorsReducer } from "./errors-slice";
-
-export type { UIState, EntitiesState, ErrorsState };
-
-export {
-  uiActions,
-  uiReducer,
-  entitiesActions,
-  entitiesReducer,
-  errorsActions,
-  errorsReducer,
-};
+export * from "./ui-slice";
+export * from "./entities-slice";
+export * from "./errors-slice";
