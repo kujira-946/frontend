db479adc5ce0b7e83db5549a3be615b929c89768
Sekkacchi <sekkacchi@gmail.com>
2023-04-03 13:08:18 -0500

    Create base logbooks page.

---
+++ src/pages/dashboard/logbooks.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import * as Constants from "@/utils/constants";
+import * as Functions from "@/utils/functions";
+
+const Logbooks = () => {
+  const { currentUser } = Functions.useEntitiesSlice();
+
+  const router = useRouter();
+  useEffect(() => {
+    if (!currentUser) router.push(Constants.ClientRoutes.LANDING);
+  }, [currentUser]);
+
+  return (
+    <>
+      <Head>
+        <title>Kujira | Logbooks</title>
+        <meta name="description" content="Kujira app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <>Foo</>
+    </>
+  );
+};
+
+export default Logbooks;
