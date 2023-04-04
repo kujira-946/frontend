0c6b214ef9ade6d2747828efcdd9637c0637e617
Sekkacchi <sekkacchi@gmail.com>
2023-04-04 14:59:07 -0500

    Create overview-groups.tsx component. Fetch overview groups when overview loads.

--- src/components/dashboard/overview-groups.tsx
+++ src/components/dashboard/overview-groups.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
+import * as Functions from "@/utils/functions";
 import * as Styles from "@/utils/styles";
+import { useEffect } from "react";
+import { fetchOverviewOverviewGroupsRequest } from "@/sagas/overview-groups.saga";
 
 // ========================================================================================= //
 // [ STYLED COMPONENTS ] =================================================================== //
@@ -20,5 +23,16 @@ const Container = styled.section`
 // ========================================================================================= //
 
 export const OverviewGroups = () => {
+  const dispatch = Functions.useAppDispatch();
+
+  const { overviews, overviewGroups } = Functions.useEntitiesSlice();
+
+  useEffect(() => {
+    if (overviews && !overviewGroups) {
+      const overview = Object.values(overviews)[0];
+      dispatch(fetchOverviewOverviewGroupsRequest(overview.id));
+    }
+  }, [overviews, overviewGroups]);
+
   return <Container></Container>;
 };
