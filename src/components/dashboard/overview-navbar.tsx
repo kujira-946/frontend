c18099664e72c16760bcd048c0c0d632560a2196
Sekkacchi <sekkacchi@gmail.com>
2023-04-06 11:01:53 -0500

    Optimize dashboard layout navbar.

--- src/components/dashboard/overview-navbar.tsx
+++ src/components/dashboard/overview-navbar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import { memo } from "react";
-import { useSignal } from "@preact/signals-react";
 
 import * as Icons from "@/components/icons";
 import * as Constants from "@/utils/constants";
