408e50384ce99af90764acb42c4c3436d1dfcd34
Sekkacchi <sekkacchi@gmail.com>
2023-04-17 14:29:43 -0500

    Add redirects.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,40 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/logbook",
+        destination: "/dashboard/logbooks",
+        permanent: true,
+      },
+      {
+        source: "/logbooks",
+        destination: "/dashboard/logbooks",
+        permanent: true,
+      },
+      {
+        source: "/review",
+        destination: "/dashboard/reviews",
+        permanent: true,
+      },
+      {
+        source: "/reviews",
+        destination: "/dashboard/reviews",
+        permanent: true,
+      },
+      {
+        source: "/setting",
+        destination: "/dashboard/settings",
+        permanent: true,
+      },
+      {
+        source: "/settings",
+        destination: "/dashboard/settings",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
